d4e997dfad66434be75ea97846d8337b4be5240e
droid <droidsareut@proton.me>
2025-08-29 15:44:28 -0400

    Add extra measures for teleport to spawn

--- behaviors/scripts/customCommands.js
+++ behaviors/scripts/customCommands.js
@@ -9,7 +9,7 @@ import {
     GameMode, 
     ScriptEventSource, 
     system, 
-    world 
+    world
 } from "@minecraft/server";
 
 import { ActionFormData } from "@minecraft/server-ui";
@@ -23,23 +23,27 @@ system.beforeEvents.startup.subscribe(event => {
         },
         (origin) => {
             if (origin.sourceEntity.hasTag("inGame")) {
-                origin.sourceEntity.sendMessage("§cYou cannot return to spawn while in game!");
+                system.run(() => origin.sourceEntity.playSound("random.break"));
+                origin.sourceEntity.sendMessage("§vYou cannot return to spawn while in game!");
             
                 return { status: CustomCommandStatus.Success };
-            };
-            
-            system.run(() => {
-                origin.sourceEntity.teleport({
-                    x: 0.50, y: -59, z: 0.50, 
-                }, { facingLocation: {
-                    x: 0.50, y: -59, z: 1.50,
-                } });
-            
-                origin.sourceEntity.sendMessage("§aSuccessfully teleported to spawn.");
-                //system.runTimeout(origin.sourceEntity.playSound("mob.endermen.portal"), 1)
-            });
-            
-            return { status: CustomCommandStatus.Success };
+            } else if (origin.sourceEntity.hasTag("spectatingGame")) {
+                system.run(() => {
+                    origin.sourceEntity.removeTag("spectatingGame")
+                    origin.sourceEntity.setGameMode(GameMode.Adventure);
+                    origin.sourceEntity.teleport({ x: 0.50, y: -59, z: 0.50 }, { facingLocation: { x: 0.50, y: -59, z: 1.50} })
+                });
+                system.runTimeout(() => origin.sourceEntity.playSound("mob.endermen.portal"), 1);
+                origin.sourceEntity.sendMessage("§gExcited spectator.");
+
+                return { status: CustomCommandStatus.Success };
+            } else {
+                system.run(() => origin.sourceEntity.teleport({ x: 0.50, y: -59, z: 0.50 }, { facingLocation: { x: 0.50, y: -59, z: 1.50} }));
+                system.runTimeout(() => origin.sourceEntity.playSound("mob.endermen.portal"), 1);
+                origin.sourceEntity.sendMessage("§gTeleported to spawn.");
+
+                return { status: CustomCommandStatus.Success };
+            }
         }
     );
 
