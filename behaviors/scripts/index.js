c93e61730a68bc633e248a651ad29792ae80ac3a
droid <droidsareut@proton.me>
2025-08-30 00:24:28 -0400

    Add imports for hub files

--- behaviors/scripts/index.js
+++ behaviors/scripts/index.js
@@ -5,3 +5,10 @@ import "leaderboards";
 import "onJoin";
 import "ranks";
 import "attacking";
+
+import "hub/alfredShop";
+import "hub/effects";
+import "hub/statDisplay";
+import "hub/suggestionBox";
+import "hub/time";
+import "hub/parkour";
