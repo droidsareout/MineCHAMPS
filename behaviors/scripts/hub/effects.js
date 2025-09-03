97d353dd8329bcafca99333fe166c511e22adaae
droid <droidsareut@proton.me>
2025-09-03 00:16:42 -0400

    Add in game player saturation

--- behaviors/scripts/hub/effects.js
+++ behaviors/scripts/hub/effects.js
@@ -1,17 +1,16 @@
 import { system, world, GameMode } from "@minecraft/server";
 
 system.runInterval(() => {
-    const dimension = world.getDimension("overworld");
-    
     world.getPlayers().forEach((player) => {
+        player.addEffect("saturation", 100, { amplifier: 255, showParticles: false});
+
         if (player.hasTag("inGame")) return;
         if (player.getGameMode() === GameMode.Creative || player.getGameMode() === GameMode.Spectator) return;
 
         player.addEffect("resistance", 100, { amplifier: 255, showParticles: false});
-        player.addEffect("saturation", 100, { amplifier: 255, showParticles: false});
     });
     
-    dimension.getEntities({ typeId: "minecraft:iron_golem" }).forEach((entity) => {
+    world.getDimension("overworld").getEntities({ typeId: "minecraft:iron_golem" }).forEach((entity) => {
         entity.addEffect("instant_health", 60, {amplifier: 255, showParticles: false})
     });
 });
