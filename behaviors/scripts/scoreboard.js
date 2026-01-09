49e5859d03a16a36c9ca9aa455889d3b9086a888
Ric <ric.devs00@gmail.com>
2026-01-09 17:57:48 -0600

    Refactor and optimize scoreboard.js
    Switched score initialization to on join instead of per tick
    
    Cleaned up playtime logic and fixed a bug where incrementing minutes was delayed by one second, hours by another second, and days by one more
    
    Refactored the player count logic and removed unnecessary type coercion and iteration over players

--- behaviors/scripts/scoreboard.js
+++ behaviors/scripts/scoreboard.js
@@ -1,86 +1,66 @@
 import { world, system } from '@minecraft/server';
 
 //Default Values
-system.runInterval(() => {
-    world.getPlayers().forEach((player) => {
-        const scoreboard = world.scoreboard;
-        const scoreboards = [
-            'kills',
-            'deaths',
-            'wins',
-            'gold',
-            'lifetimeGold',
+world.afterEvents.playerJoin.subscribe((event) => {
+    const player = world.getEntity(event.playerId);
+    const scoreboard = world.scoreboard;
+    const objectiveIds = [
+        'kills',
+        'deaths',
+        'wins',
+        'gold',
+        'lifetimeGold',
 
-            'ptSec',
-            'ptMin',
-            'ptHour',
-            'ptDay',
-        ];
+        'ptSec',
+        'ptMin',
+        'ptHour',
+        'ptDay',
+    ];
 
-        for (let i = 0; i < scoreboards.length; i++) {
-            if (
-                scoreboard.getObjective(scoreboards[i]).getScore(player) ===
-                undefined
-            ) {
-                scoreboard.getObjective(scoreboards[i]).addScore(player, 0);
-            }
-        }
-    });
+    for (const id of objectiveIds) {
+        const objective = scoreboard.getObjective(id);
+        if (objective === undefined) continue;
+        objective.addScore(player, 0);
+    }
 });
 
 //Play Time
 system.runInterval(() => {
-    for (const player of world.getPlayers()) {
-        world.scoreboard.getObjective('ptSec').addScore(player, 1);
-    }
-}, 20);
-system.runInterval(() => {
-    for (const player of world.getPlayers()) {
-        const ptSec = world.scoreboard.getObjective('ptSec');
-        const ptMin = world.scoreboard.getObjective('ptMin');
-        const ptHour = world.scoreboard.getObjective('ptHour');
-        const ptDay = world.scoreboard.getObjective('ptDay');
+    const ptSec =  world.scoreboard.getObjective('ptSec');
+    const ptMin =  world.scoreboard.getObjective('ptMin');
+    const ptHour = world.scoreboard.getObjective('ptHour');
+    const ptDay =  world.scoreboard.getObjective('ptDay');
 
+    for (const player of world.getAllPlayers()) {
+        ptSec.addScore(player, 1);
         if (ptSec.getScore(player) >= 60) {
             ptSec.setScore(player, 0);
             ptMin.addScore(player, 1);
-        } else if (ptMin.getScore(player) >= 60) {
-            ptMin.setScore(player, 0);
-            ptHour.addScore(player, 1);
-        } else if (ptHour.getScore(player) >= 24) {
-            ptHour.setScore(player, 0);
-            ptDay.addScore(player, 1);
+            if (ptMin.getScore(player) >= 60) {
+                ptMin.setScore(player, 0);
+                ptHour.addScore(player, 1);
+                if (ptHour.getScore(player) >= 24) {
+                    ptHour.setScore(player, 0);
+                    ptDay.addScore(player, 1);
+                }
+            }
         }
     }
-});
+}, 20);
 
 //Player counts
 system.runInterval(() => {
-    world.getPlayers().forEach((player) => {
-        const worldStats = world.scoreboard.getObjective('worldStats');
-        let onlineCount = Number(JSON.stringify(world.getAllPlayers().length));
-        let inGameCount = Number(
-            JSON.stringify(
-                player.dimension.getPlayers({ tags: ['inGame'] }).length,
-            ),
-        );
-        let spectatingGameCount = Number(
-            JSON.stringify(
-                player.dimension.getPlayers({ tags: ['spectatingGame'] })
-                    .length,
-            ),
-        );
-        let optedInCount = Number(
-            JSON.stringify(
-                player.dimension.getPlayers({ tags: ['optedIn'] }).length,
-            ),
-        );
-        let optedOutCount = onlineCount - optedInCount;
+    const overworld = world.getDimension('overworld');
+    const worldStats = world.scoreboard.getObjective('worldStats');
+    const onlineCount = world.getAllPlayers().length;
+    const inGameCount = overworld.getPlayers({ tags: ['inGame'] }).length;
+    const spectatingGameCount = overworld.getPlayers({ tags: ['spectatingGame'] }).length;
+    const optedInCount = overworld.getPlayers({ tags: ['optedIn'] }).length;
+    const optedOutCount = onlineCount - optedInCount;
 
-        worldStats.setScore('online', onlineCount);
-        worldStats.setScore('inGame', inGameCount);
-        worldStats.setScore('spectatingGame', spectatingGameCount);
-        worldStats.setScore('optedIn', optedInCount);
-        worldStats.setScore('optedOut', optedOutCount);
-    });
+    worldStats.setScore('online', onlineCount);
+    worldStats.setScore('inGame', inGameCount);
+    worldStats.setScore('spectatingGame', spectatingGameCount);
+    worldStats.setScore('optedIn', optedInCount);
+    worldStats.setScore('optedOut', optedOutCount);
 });
